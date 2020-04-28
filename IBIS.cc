a42a7bfdbfd8b6dc1f3ef25872ee930d582971d1
Amy Williams <alw289@cornell.edu>
2020-04-28 16:07:43 -0400

    v1.19.3: set genetic map (1 cM/Mb) if none given

--- IBIS.cc
+++ IBIS.cc
@@ -1169,8 +1169,8 @@ void printUsageAndExit(){
 
 int main(int argc, char **argv) {
 
-	const char* VERSION_NUMBER = "1.19.2";
-	const char* RELEASE_DATE = "March 9, 2020";
+	const char* VERSION_NUMBER = "1.19.3";
+	const char* RELEASE_DATE = "April 28, 2020";
 	printf("IBIS Segment Caller!  v%s    (Released %s)\n\n", VERSION_NUMBER, RELEASE_DATE);
 
 	uint64_t numIndivs, numMarkers;//counts of input quantities.
